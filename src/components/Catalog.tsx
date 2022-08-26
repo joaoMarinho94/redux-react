74c2a30a5f6633a27adc87bf0f66f03b0d57e280
joaoMarinho94 <joaopaulim94@gmail.com>
2022-08-26 01:54:47 -0300

    feat: create fake server json

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,9 +1,28 @@
-import { useSelector } from 'react-redux';
+import api from '@/services/api';
+import { useEffect, useState } from 'react';
+
+import { IProduct } from 'store/modules/cart/types';
 
 export default function Catalog() {
-  const store = useSelector((state) => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log('store: ', store);
+  useEffect(() => {
+    api.get('/products').then(({ data }) => {
+      setCatalog(data);
+    });
+  }, []);
 
-  return <h1>Catalog</h1>;
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {' - '}
+          <span>{product.price}</span> {'  '}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </main>
+  );
 }
