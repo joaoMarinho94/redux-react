97679b259897c9d7a52432d3921108e69e520291
joaoMarinho94 <joaopaulim94@gmail.com>
2022-09-03 01:04:29 -0300

    feat: add setup redux sagas

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,24 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddelware from 'redux-saga';
 
 import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
 export interface IState {
   cart: ICartState;
 }
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddelware();
+
+const middleware = [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
